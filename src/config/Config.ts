f2af5373e7ef965c45c7838daf9435692c053005
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-07 00:55:22 -0300

    chore: add application Config

---
+++ src/config/Config.ts
@@ -0,0 +1,12 @@
+function getConfig<T extends string> (configName: string): T {
+  const config = process.env[configName]
+  if (config === undefined) {
+    throw new Error(`Missing config ${configName}`)
+  }
+
+  return config as T
+}
+
+export const config = {
+  host: getConfig('REACT_APP_HOST')
+}
