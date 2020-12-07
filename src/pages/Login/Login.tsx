deb6bae47ee7946b341aa7b4bb57be8fa2ddd2b2
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-07 00:56:56 -0300

    chore(Login): add login logic

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { Input, Button } from 'antd'
+import React, { useEffect } from 'react'
+import { Input, Button, Typography } from 'antd'
 import { LoginContainer, LoginForm } from './styles'
+import { useHistory } from 'react-router-dom'
+import { useLogin } from '../../hooks/useLogin'
 
-export function Login () {
+const { Text } = Typography
+
+export default function Login () {
+  const { login, loginError, loginLoading, onChange, loggedIn } = useLogin()
+  const history = useHistory()
+
+  useEffect(() => {
+    if (loggedIn) {
+      history.push('/home')
+    }
+  }, [loggedIn])
+
+  function onSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    login()
+  }
+
+  if (loginLoading) {
+    return <LoginContainer>
+      <div>Loading...</div>
+    </LoginContainer>
+  }
+
   return (
     <LoginContainer>
-    <LoginForm>
-      <Input type="email" placeholder="Usuário" required />
-      <Input placeholder="Senha" required />
+    <LoginForm onSubmit={onSubmit}>
+      <Input
+        onChange={onChange}
+        name="email"
+        type="email"
+        placeholder="Usuário"
+        autoComplete="email"
+        required />
+
+      <Input
+        onChange={onChange}
+        name="password"
+        type="password"
+        placeholder="Senha"
+        autoComplete="password"
+        required />
 
       <Button type="primary" htmlType="submit">
         Login
       </Button>
+
+      {loginError && <Text type="danger">{loginError.message}</Text>}
     </LoginForm>
     </LoginContainer>
   )
