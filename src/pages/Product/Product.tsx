b8c673b4389b4079e0c084c12023efd5198dd20b
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-19 23:35:28 -0300

    chore: add Product page

---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Page } from '../../components/Page/Page.component'
+
+export default function Product () {
+  return (
+    <Page title="Produto" />
+  )
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,6 +8,7 @@ import { PageLoader } from '../components/Page/PageLoader.component'
 /* Pages */
 const Login = lazy(() => import('../pages/Login'))
 const Home = lazy(() => import('../pages/Home'))
+const Product = lazy(() => import('../pages/Product'))
 
 export function Routes () {
   return (
@@ -18,6 +19,7 @@ export function Routes () {
           <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/home" component={Home} />
+            <Route exact path="/product" component={Product} />
           </Switch>
         </Suspense>
       </HandleExternalRoute>
