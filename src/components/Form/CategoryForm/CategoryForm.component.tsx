14764a58d836c364769650f4700c563fb8eb831a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 19:56:17 -0300

    chore: add CategoryForm

---
+++ src/components/Form/CategoryForm/CategoryForm.component.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent } from 'react'
+import { Input, Button, Form } from 'antd'
+
+export interface ICategoryFormProps {
+    categoryName: string;
+    description?: string;
+    loading: boolean;
+    onChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export function CategoryForm ({ categoryName, description, onChange, onSubmit, loading }: ICategoryFormProps) {
+  return <Form title="Adicionar nova categoria" layout="inline" onSubmitCapture={onSubmit}>
+      <Form.Item>
+        <Input onChange={onChange} value={categoryName} name="name" title="Nome" placeholder="Nome" required />
+      </Form.Item>
+      <Form.Item>
+        <Input.TextArea cols={50} rows={1} onChange={onChange} value={description} name="description" showCount maxLength={250} title="Descrição" placeholder="Descrição opcional" />
+      </Form.Item>
+      <Button loading={loading} type="primary" htmlType="submit">Salvar</Button>
+    </Form>
+}
