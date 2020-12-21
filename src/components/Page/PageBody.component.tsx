276dc60c55f0179cd565be72881607917050a74c
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 19:43:07 -0300

    chore: add PageBody component

---
+++ src/components/Page/PageBody.component.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const PageBody = styled.div`
+  padding: 0 1.5rem;
+  padding-top: 1.5rem;
+  background-color: rgb(240, 242, 245);
+  height: 100%;
+  width: 100%;
+`
