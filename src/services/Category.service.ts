889219d33903ed38b007f12c6e3d4020dc74ab2b
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 19:52:14 -0300

    chore: add Category service

---
+++ src/services/Category.service.ts
@@ -0,0 +1,19 @@
+import httpRequest from '../adapters/HttpRequest'
+
+export interface ICategory {
+  id: string
+  name: string
+  description?: string
+}
+
+export const categoryService = {
+  create (newCategory: Pick<ICategory, 'name' |'description'>) {
+    return httpRequest.post<string>('/category', newCategory)
+  },
+  list () {
+    return httpRequest.get<ICategory[]>('/category')
+  },
+  delete (id: string) {
+    return httpRequest.delete<never>('/category/' + id)
+  }
+}
