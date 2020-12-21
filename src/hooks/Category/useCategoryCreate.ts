9f5f3a421be5ad8de4634bcf34e33871ebb33dac
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-21 19:53:44 -0300

    chore: add useCategoryCreate

---
+++ src/hooks/Category/useCategoryCreate.ts
@@ -0,0 +1,53 @@
+import { useState, ChangeEvent } from 'react'
+import { categoryService } from '../services/Category.service'
+import { tap, catchError } from 'rxjs/operators'
+import { throwError } from 'rxjs'
+
+export function useCategoryCreate () {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState<string>()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<any>(null)
+
+  function onSuccess () {
+    setName('')
+    setDescription(undefined)
+    setLoading(false)
+  }
+
+  function onError (error: any) {
+    setLoading(false)
+    setError(error)
+    return throwError(error)
+  }
+
+  function saveCategory () {
+    setLoading(true)
+    setError(null)
+
+    return categoryService
+      .create({ name, description })
+      .pipe(tap(onSuccess))
+      .pipe(catchError(onError))
+  }
+
+  function onChange (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const { name, value } = e.target
+    if (name === 'name') {
+      return setName(value)
+    }
+
+    if (name === 'description') {
+      return setDescription(value)
+    }
+  }
+
+  return {
+    categoryName: name,
+    description,
+    onChange,
+    saveCategory,
+    categoryLoading: loading,
+    categoryError: error
+  }
+}
