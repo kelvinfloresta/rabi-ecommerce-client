b0835ae378d8bce0467626374579037fb57abd60
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:15:49 -0300

    chore(Routes): remove category route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,18 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import ScrollToTop from './ScrollToTop'
-import { HandleExternalRoute } from './HandleExternalRoute'
 import { PageLoader } from '../components/Page/PageLoader.component'
+
 import AuthRoute from './AuthRoute'
+import { HandleExternalRoute } from './HandleExternalRoute'
+import ScrollToTop from './ScrollToTop'
 
 /* Pages */
 const Login = lazy(() => import('../pages/Login'))
 const Home = lazy(() => import('../pages/Home'))
-const Product = lazy(() => import('../pages/Product'))
-const Category = lazy(() => import('../pages/Category'))
+const Product = lazy(() => import('../pages/Product/Product.page'))
 
-export function Routes () {
+export function Routes() {
   return (
     <Router>
       <ScrollToTop />
@@ -22,7 +22,6 @@ export function Routes () {
             <Route exact path="/" component={Login} />
             <AuthRoute exact path="/home" component={Home} />
             <AuthRoute exact path="/product" component={Product} />
-            <AuthRoute exact path="/category" component={Category} />
           </Switch>
         </Suspense>
       </HandleExternalRoute>
