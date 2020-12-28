6aa5eeec025099d05578724f25a0fbd5cace32f6
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:18:23 -0300

    style(AuthRoute): change code style

--- src/routes/AuthRoute.tsx
+++ src/routes/AuthRoute.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+
 import { authService } from '../services/Auth.service'
 
-export default function AuthRoute ({
-  component: Component,
-  ...rest
-}: any) {
+export default function AuthRoute({ component: Component, ...rest }: any) {
   return (
     <Route
       {...rest}
       render={props =>
-        authService.restoreSession() ? <Component {...props} /> : <Redirect to="/" />
+        authService.restoreSession() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
       }
     />
   )
