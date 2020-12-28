3ba63bc429390d61548afd4fb4bd94576282832f
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:12:19 -0300

    chore: add hooks interface

---
+++ src/hooks/IHooks.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs'
+
+export type ICreateObservable<TInput, TOutput> = (
+  param: TInput,
+) => Observable<TOutput>
+
+export type IDeleteObservable = (param: string) => Observable<never>
+
+export type IListObservable<TList, TFilter> = (
+  param: TFilter,
+) => Observable<TList[]>
+
+export type IPatchObservable<TFilter, TData> = (
+  filter: TFilter,
+  data: TData,
+) => Observable<never>
