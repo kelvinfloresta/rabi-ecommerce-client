b68096c830dfde3a534965f18ae9d4eb57ccf5ca
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:21:47 -0300

    chore(PageComponent): add footer and extra

--- src/components/Page/Page.component.tsx
+++ src/components/Page/Page.component.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
+
 import { Header } from '../Header'
+import { IHeaderProps } from '../Header/Header'
+
 import { PageContainer } from './Page.style'
 import { PageBody } from './PageBody.component'
 
-interface IPageProps {
-    title: string
-    children?: React.ReactNode
+interface IPageProps extends IHeaderProps {
+  children?: React.ReactNode
 }
 
-export function Page ({ title, children }: IPageProps) {
+export function Page({ title, extra, footer, children }: IPageProps) {
   return (
     <PageContainer>
-      <Header title={title}/>
-      <PageBody>
-        {children}
-      </PageBody>
+      <Header title={title} extra={extra} footer={footer} />
+      <PageBody>{children}</PageBody>
     </PageContainer>
   )
 }
