a58e3f2a5c3c3d2707fcb0fce2f59d48a8740596
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:10:54 -0300

    chore: add Flex component

--- src/components/Flex/Flex.component.ts
+++ src/components/Flex/Flex.component.ts
@@ -1,23 +1,23 @@
 import styled from 'styled-components'
 
 export interface IFlexItemProps {
-  flexGrow?: number,
-  flexShrink?: number,
-  flexBasis?: number,
-  flex?: string,
+  flexGrow?: number
+  flexShrink?: number
+  flexBasis?: number
+  flex?: string
 }
 
 export interface IFlexLayoutProps {
-  padding?: string,
-  margin?: string,
-  width?: string,
-  height?: string,
-  maxWidth?: string,
+  padding?: string
+  margin?: string
+  width?: string
+  height?: string
+  maxWidth?: string
   maxHeight?: string
 }
 
 export interface IFlexProps extends IFlexItemProps, IFlexLayoutProps {
-  flexDirection?: 'row' | 'column',
+  flexDirection?: 'row' | 'column'
   justifyContent?:
     | 'flex-start'
     | 'flex-end'
@@ -25,8 +25,8 @@ export interface IFlexProps extends IFlexItemProps, IFlexLayoutProps {
     | 'space-between'
     | 'space-around'
     | 'initial'
-    | 'inherit',
-  flexWrap?: 'wrap' | 'nowrap' | 'wrap-reverse',
+    | 'inherit'
+  flexWrap?: 'wrap' | 'nowrap' | 'wrap-reverse'
   alignItems?:
     | 'stretch'
     | 'center'
@@ -34,21 +34,21 @@ export interface IFlexProps extends IFlexItemProps, IFlexLayoutProps {
     | 'flex-end'
     | 'baseline'
     | 'initial'
-    | 'inherit',
+    | 'inherit'
 }
 
 export const Flex = styled.div<IFlexProps>`
   display: flex;
-  justify-content: ${props => props.justifyContent || 'flex-start'};
-  flex-direction: ${props => props.flexDirection || 'row'};
-  flex-grow: ${props => props.flexGrow || 0};
-  flex-basis: ${props => props.flexBasis || 'auto'};
-  flex-shrink: ${props => props.flexShrink || 1};
-  flex-wrap: ${props => props.flexWrap || 'nowrap'};
-  flex: ${props => props.flex || '0 1 auto'};
-  align-items: ${props => props.alignItems || 'stretch'};
-  margin: ${props => props.margin || '0'};
-  padding: ${props => props.padding || '0'};
-  height: ${props => props.height || 'auto'};
-  max-width: ${props => props.maxWidth || 'none'};
+  justify-content: ${(props) => props.justifyContent || 'flex-start'};
+  flex-direction: ${(props) => props.flexDirection || 'row'};
+  flex-grow: ${(props) => props.flexGrow || 0};
+  flex-basis: ${(props) => props.flexBasis || 'auto'};
+  flex-shrink: ${(props) => props.flexShrink || 1};
+  flex-wrap: ${(props) => props.flexWrap || 'nowrap'};
+  flex: ${(props) => props.flex || '0 1 auto'};
+  align-items: ${(props) => props.alignItems || 'stretch'};
+  margin: ${(props) => props.margin || '0'};
+  padding: ${(props) => props.padding || '0'};
+  height: ${(props) => props.height || 'auto'};
+  max-width: ${(props) => props.maxWidth || 'none'};
 `
