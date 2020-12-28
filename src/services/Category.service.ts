98d099f2d8784da10c517a33f1e36ac0f8205b9c
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:26:45 -0300

    refactor(CategoryService): add interface ICreateCategoryInput

--- src/services/Category.service.ts
+++ src/services/Category.service.ts
@@ -6,14 +6,19 @@ export interface ICategory {
   description?: string
 }
 
+export interface ICreateCategoryInput {
+  name: string
+  description?: string
+}
+
 export const categoryService = {
-  create (newCategory: Pick<ICategory, 'name' |'description'>) {
+  create(newCategory: ICreateCategoryInput) {
     return httpRequest.post<string>('/category', newCategory)
   },
-  list () {
+  list() {
     return httpRequest.get<ICategory[]>('/category')
   },
-  delete (id: string) {
+  delete(id: string) {
     return httpRequest.delete<never>('/category/' + id)
-  }
+  },
 }
