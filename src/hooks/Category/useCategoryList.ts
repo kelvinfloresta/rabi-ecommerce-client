1507f5f7396ccfb57f163c94a21e57745e1678eb
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:23:07 -0300

    style(useCategoryList): change code style

--- src/hooks/Category/useCategoryList.ts
+++ src/hooks/Category/useCategoryList.ts
@@ -1,38 +1,36 @@
 import { useState } from 'react'
-import { categoryService, ICategory } from '../../services/Category.service'
-import { tap, catchError } from 'rxjs/operators'
 import { throwError } from 'rxjs'
+import { tap, catchError } from 'rxjs/operators'
 
-export function useCategoryList () {
+import { categoryService, ICategory } from '../../services/Category.service'
+
+export function useCategoryList() {
   const [categories, setCategories] = useState<ICategory[]>([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<any>(null)
 
-  function onSuccess (categories: ICategory[]) {
+  function onSuccess(categories: ICategory[]) {
     setCategories(categories)
     setLoading(false)
   }
 
-  function onError (error: any) {
+  function onError(error: any) {
     setLoading(false)
     setError(error)
     return throwError(error)
   }
 
-  function listCategory () {
+  function listCategory() {
     setLoading(true)
     setError(null)
 
-    return categoryService
-      .list()
-      .pipe(tap(onSuccess))
-      .pipe(catchError(onError))
+    return categoryService.list().pipe(tap(onSuccess)).pipe(catchError(onError))
   }
 
   return {
     categories,
     listCategory,
     listCategoryLoading: loading,
-    listCategoryError: error
+    listCategoryError: error,
   }
 }
