d8a222249f4ab3c21aa058b162eb199cb89f3ea0
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-28 19:08:29 -0300

    chore(IHttpRequest): add patch method

--- src/adapters/HttpRequest/AxiosObservable.adapter.ts
+++ src/adapters/HttpRequest/AxiosObservable.adapter.ts
@@ -5,15 +5,15 @@ import { catchError, map } from 'rxjs/operators'
 import IHttpRequest from './IHttpRequest.adapter'
 
 export default class AxiosObservableAdapter implements IHttpRequest {
-  httpRequest: AxiosObservable;
+  httpRequest: AxiosObservable
 
-  constructor (baseURL: string) {
+  constructor(baseURL: string) {
     this.httpRequest = AxiosObservable.create({
-      baseURL
+      baseURL,
     })
   }
 
-  isAuthenticated (): boolean {
+  isAuthenticated(): boolean {
     return this.httpRequest.defaults.headers.Authorization?.length > 0
   }
 
@@ -21,28 +21,35 @@ export default class AxiosObservableAdapter implements IHttpRequest {
     this.httpRequest.defaults.headers.Authorization = token
   }
 
-  onError (error: any) {
+  onError(error: any) {
     if (error?.response?.data) {
       return throwError(error.response.data)
     }
     return throwError(error)
   }
 
-  post<TResponse, TBody> (url: string, data?: TBody) {
+  post<TResponse, TBody>(url: string, data?: TBody) {
     return this.httpRequest
       .post<TResponse>(url, data)
       .pipe(map(response => response.data))
       .pipe(catchError(this.onError))
   }
 
-  get<T> (url: string) {
+  patch<TResponse, TBody>(url: string, data?: TBody) {
+    return this.httpRequest
+      .patch<TResponse>(url, data)
+      .pipe(map(response => response.data))
+      .pipe(catchError(this.onError))
+  }
+
+  get<T>(url: string) {
     return this.httpRequest
       .get<T>(url)
       .pipe(map(response => response.data))
       .pipe(catchError(this.onError))
   }
 
-  delete<TResponse> (url: string): Observable<TResponse> {
+  delete<TResponse>(url: string): Observable<TResponse> {
     return this.httpRequest
       .delete<TResponse>(url)
       .pipe(map(response => response.data))
--- src/adapters/HttpRequest/IHttpRequest.adapter.ts
+++ src/adapters/HttpRequest/IHttpRequest.adapter.ts
@@ -2,6 +2,10 @@ import { Observable } from 'rxjs'
 
 export default interface IHttpRequest {
   post<TResponse, TBody = any>(url: string, body?: TBody): Observable<TResponse>
+  patch<TResponse, TBody = any>(
+    url: string,
+    body?: TBody,
+  ): Observable<TResponse>
   get<TResponse>(url: string): Observable<TResponse>
   delete<TResponse>(url: string): Observable<TResponse>
   setToken(token: string): void
