81d75890f8cfeee466256de4fdaf8274f8dd43d3
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:15:11 -0300

    style: enforces code style

--- src/components/Page/PageLoader.component.tsx
+++ src/components/Page/PageLoader.component.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
 import { Spin } from 'antd'
+import React from 'react'
 import styled from 'styled-components'
 
 const PageLoaderContainer = styled.div`
-    min-height: 100%;
-    width: 100%;
-    background-color: rgb(240, 242, 245);
-    display: flex;
-    padding-top: 10%;
-    justify-content: center;
-    animation: fadeIn .3s;
+  min-height: 100%;
+  width: 100%;
+  background-color: rgb(240, 242, 245);
+  display: flex;
+  padding-top: 10%;
+  justify-content: center;
+  animation: fadeIn 0.3s;
 `
 
-export function PageLoader () {
-  return <PageLoaderContainer>
-    <Spin size="large" />
-  </PageLoaderContainer>
+export function PageLoader() {
+  return (
+    <PageLoaderContainer>
+      <Spin size="large" />
+    </PageLoaderContainer>
+  )
 }
