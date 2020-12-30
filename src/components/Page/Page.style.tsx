c10cb68569cfc88927e5aadcdb8d83753b03dabb
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:38:14 -0300

    chore(Page): increase height on small devices

--- src/components/Page/Page.style.tsx
+++ src/components/Page/Page.style.tsx
@@ -3,5 +3,7 @@ import styled from 'styled-components'
 export const PageContainer = styled.div`
   overflow: hidden;
   width: 100%;
-  animation: fadeIn .3s;
+  animation: fadeIn 0.3s;
+  min-height: 100vh;
+  padding-bottom: 1rem;
 `
