ecac7302648da58cb089db05d78d44317c9529d3
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:14:44 -0300

    refactor(Product hooks): use new HOF hooks

---
+++ src/hooks/Product.hook.ts
@@ -0,0 +1,27 @@
+import {
+  productService,
+  ICreateProductInput,
+} from '../services/Product.service'
+import { IEmpty } from '../utils/Empty.util'
+
+import { makeUseActive } from './CRUD/makeUseActive.hook'
+import { makeUseDelete } from './CRUD/makeUseDelete.hook'
+import { makeUseForm } from './CRUD/makeUseForm.hook'
+import { makeUseList } from './CRUD/makeUseList.hook'
+
+export const useActiveProduct = makeUseActive(productService.patch)
+
+export const useDeleteProduct = makeUseDelete(productService.delete)
+
+export const useProductList = makeUseList(productService.list, undefined)
+
+const EMPTY_PRODUCT: IEmpty<ICreateProductInput> = {
+  name: '',
+  price: 1,
+  description: '',
+  active: true,
+  categoryId: null,
+  isEmpty: true,
+}
+
+export const useProductForm = makeUseForm(productService.create, EMPTY_PRODUCT)
