4e34e2734f0de2f2fb7b9f2967c943a465ebeef9
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:45:18 -0300

    style: enforces code style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import 'antd/dist/antd.css'
@@ -9,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
 
 // If you want to start measuring performance in your app, pass a function
