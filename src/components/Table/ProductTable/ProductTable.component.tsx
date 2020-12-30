55e0b40f633355a4cdb7709a184d9aed2273a416
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:41:06 -0300

    chore: add ProductTable

---
+++ src/components/Table/ProductTable/ProductTable.component.tsx
@@ -0,0 +1,81 @@
+import { ColumnsType } from 'antd/lib/table'
+import React from 'react'
+import { Observable } from 'rxjs'
+
+import { TableData } from '../../../frameworks/components/TableData.component'
+import { useActiveColumn } from '../../../frameworks/hooks/useActiveColumn'
+import { useDeleteColumn } from '../../../frameworks/hooks/useDeleteColumn'
+import { IMultipleLoading } from '../../../hooks/CRUD/ICRUD.hook'
+import { IProduct } from '../../../services/Product.service'
+import { formatCurrency } from '../../../utils/Format.util'
+
+interface IProductTableProps {
+  products: IProduct[]
+  loading: boolean
+  error: any
+
+  onActive(id: string, active: boolean): void
+  activeLoading: IMultipleLoading
+
+  onDelete(id: string): Observable<never>
+  deleteLoading: boolean
+}
+
+const defaultColumns: ColumnsType<IProduct> = [
+  {
+    title: 'Nome',
+    width: '20%',
+    dataIndex: 'name',
+    sortOrder: 'ascend',
+    showSorterTooltip: false,
+    sortDirections: [],
+    sorter: (a, b) => {
+      const result = a.name.localeCompare(b.name)
+      if (result !== 0) return result
+      return a.id.localeCompare(b.id)
+    },
+  },
+  {
+    title: 'Preço',
+    dataIndex: 'price',
+    render: formatCurrency,
+  },
+  {
+    title: 'Categoria',
+    dataIndex: 'categoryName',
+  },
+  {
+    title: 'Descrição',
+    dataIndex: 'description',
+  },
+]
+
+export function ProductTable({
+  products,
+  loading,
+  error,
+  onActive,
+  activeLoading,
+  onDelete,
+  deleteLoading,
+}: IProductTableProps) {
+  const deleteColumn = useDeleteColumn<IProduct>({
+    deleteLoading,
+    onDelete,
+  })
+
+  const activeColumn = useActiveColumn<IProduct>({
+    activeLoading,
+    onActive,
+  })
+
+  const allColumns = [...defaultColumns, activeColumn, deleteColumn]
+  return (
+    <TableData
+      columns={allColumns}
+      dataSource={products}
+      error={error}
+      loading={loading}
+    />
+  )
+}
