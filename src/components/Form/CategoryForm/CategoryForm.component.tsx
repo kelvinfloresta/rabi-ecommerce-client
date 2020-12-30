58aad1c87e5aee15ca51120ff383435196464b0e
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:25:23 -0300

    refactor(CategoryForm): use new type IFormProps

--- src/components/Form/CategoryForm/CategoryForm.component.tsx
+++ src/components/Form/CategoryForm/CategoryForm.component.tsx
@@ -1,22 +1,52 @@
-import React, { ChangeEvent } from 'react'
 import { Input, Button, Form } from 'antd'
+import React from 'react'
 
-export interface ICategoryFormProps {
-    categoryName: string;
-    description?: string;
-    loading: boolean;
-    onChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-    onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-}
+import { ICreateCategoryInput } from '../../../services/Category.service'
+import { IFormProps } from '../IForm'
 
-export function CategoryForm ({ categoryName, description, onChange, onSubmit, loading }: ICategoryFormProps) {
-  return <Form autoComplete="off" title="Adicionar nova categoria" layout="inline" onSubmitCapture={onSubmit}>
-      <Form.Item>
-        <Input onChange={onChange} value={categoryName} name="name" title="Nome" placeholder="Nome" required />
+export function CategoryForm({
+  onCreate,
+  form,
+  onChange,
+  loading,
+}: IFormProps<ICreateCategoryInput>) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    onCreate && onCreate()
+  }
+
+  return (
+    <Form
+      autoComplete="off"
+      title="Adicionar nova categoria"
+      layout="inline"
+      onSubmitCapture={onSubmit}>
+      <Form.Item label="Nome" required>
+        <Input
+          onChange={onChange}
+          value={form.name}
+          name="name"
+          title="Nome"
+          placeholder="Nome"
+          required
+        />
       </Form.Item>
-      <Form.Item>
-        <Input.TextArea cols={50} rows={1} onChange={onChange} value={description} name="description" showCount maxLength={250} title="Descrição" placeholder="Descrição opcional" />
+      <Form.Item label="Descrição">
+        <Input.TextArea
+          cols={50}
+          rows={1}
+          onChange={onChange}
+          value={form.description}
+          name="description"
+          showCount
+          maxLength={250}
+          title="Descrição"
+          placeholder="Descrição opcional"
+        />
       </Form.Item>
-      <Button loading={loading} type="primary" htmlType="submit">Salvar</Button>
+      <Button loading={loading} type="primary" htmlType="submit">
+        Salvar
+      </Button>
     </Form>
+  )
 }
