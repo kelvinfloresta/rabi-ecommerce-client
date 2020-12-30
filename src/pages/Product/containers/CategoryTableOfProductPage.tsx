b8b57a7c14b7e7aa092d11eea11d45b2192538a0
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-12-30 13:42:25 -0300

    chore: add CategoryTableOfProductPage

---
+++ src/pages/Product/containers/CategoryTableOfProductPage.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect } from 'react'
+import { tap } from 'rxjs/operators'
+import { CategoryTable } from 'src/components/Table/CategoryTable/CategoryTable.component'
+import { TableToolbar } from 'src/components/Table/TableToolbar.component'
+import { ToolbarButton } from 'src/components/Toolbar'
+import { CategoryPaneContext } from 'src/contexts/ProductPage/CategoryPane.context'
+
+export function CategoryTableOfProductPage() {
+  const { listCategory, deleteCategory } = useContext(CategoryPaneContext)
+
+  useEffect(() => {
+    const subscription = listCategory.list().subscribe()
+    return () => subscription.unsubscribe()
+  }, [])
+
+  function onReload() {
+    listCategory.list().subscribe()
+  }
+
+  function onDelete(id: string) {
+    return deleteCategory
+      .del(id)
+      .pipe(tap(() => listCategory.list().subscribe()))
+  }
+
+  return (
+    <>
+      <TableToolbar>
+        <ToolbarButton
+          loading={listCategory.listLoading}
+          onClick={onReload}
+          type="reload">
+          Atualizar
+        </ToolbarButton>
+      </TableToolbar>
+      <CategoryTable
+        categories={listCategory.elements}
+        loading={listCategory.listLoading}
+        error={listCategory.listError}
+        onDelete={onDelete}
+        deleteLoading={deleteCategory.deleteLoading}
+      />
+    </>
+  )
+}
