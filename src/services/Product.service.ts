7ff306bb9a7862637a49ad0ede840ec9836ed033
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-01-10 21:33:27 -0300

    chore(ProductService): add listGroupedByCategory

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -1,4 +1,6 @@
+import _groupBy from 'lodash/groupBy'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 import httpRequest from '../adapters/HttpRequest'
 
@@ -28,12 +30,20 @@ export interface ICreateProductInput {
   readonly categoryId: string | null
 }
 
+export type IGroupedProductsByCategory = [string, IProduct[]]
+
 export const productService = {
   create(newProduct: ICreateProductInput) {
     return httpRequest.post<string>('/product', newProduct)
   },
   list(): Observable<IProduct[]> {
     return httpRequest.get<IProduct[]>('/product')
+  },
+  listGroupedByCategory() {
+    return productService.list().pipe(
+      map(x => _groupBy(x, 'categoryName')),
+      map(x => Object.entries(x)),
+    )
   },
   delete(id: string) {
     return httpRequest.delete<never>('/product/' + id)
