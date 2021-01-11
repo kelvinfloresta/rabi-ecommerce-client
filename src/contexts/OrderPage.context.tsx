74d3ab0f72eecde3935d66bc105efd6999446f14
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-01-10 21:46:53 -0300

    chore: add OrderPage context

---
+++ src/contexts/OrderPage.context.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode, useEffect } from 'react'
+import { useCartItems } from 'src/hooks/Order/useCartItems.hook'
+import { useGroupedProductsByCategory } from 'src/hooks/Product.hook'
+import { useOpen } from 'src/hooks/useOpen'
+
+import { useOrderList } from '../hooks/Order.hook'
+
+interface IOrderPageContext {
+  listOrder: ReturnType<typeof useOrderList>
+  groupedProducts: ReturnType<typeof useGroupedProductsByCategory>
+  drawer: ReturnType<typeof useOpen>
+  cart: ReturnType<typeof useCartItems>
+}
+
+export const OrderPageContext = React.createContext<IOrderPageContext>(
+  {} as any,
+)
+
+export function OrderPageProvider({ children }: { children: ReactNode }) {
+  const listOrder = useOrderList()
+  const drawer = useOpen()
+  const groupedProducts = useGroupedProductsByCategory()
+  const cart = useCartItems()
+
+  useEffect(() => {
+    if (!drawer.isOpen) {
+      return
+    }
+    const sub = groupedProducts.list().subscribe()
+    return () => sub.unsubscribe()
+  }, [drawer.isOpen])
+
+  useEffect(() => {
+    const subscription = listOrder.list().subscribe()
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return (
+    <OrderPageContext.Provider
+      value={{
+        listOrder,
+        groupedProducts,
+        drawer,
+        cart,
+      }}>
+      {children}
+    </OrderPageContext.Provider>
+  )
+}
