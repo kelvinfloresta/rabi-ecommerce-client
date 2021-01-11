f9ece130d93b59ed2f7d0ade962f49f31ee2fb0a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-01-10 21:19:19 -0300

    fix(Flex): set "initial" as default width

--- src/components/Flex/Flex.component.ts
+++ src/components/Flex/Flex.component.ts
@@ -39,16 +39,17 @@ export interface IFlexProps extends IFlexItemProps, IFlexLayoutProps {
 
 export const Flex = styled.div<IFlexProps>`
   display: flex;
-  justify-content: ${(props) => props.justifyContent || 'flex-start'};
-  flex-direction: ${(props) => props.flexDirection || 'row'};
-  flex-grow: ${(props) => props.flexGrow || 0};
-  flex-basis: ${(props) => props.flexBasis || 'auto'};
-  flex-shrink: ${(props) => props.flexShrink || 1};
-  flex-wrap: ${(props) => props.flexWrap || 'nowrap'};
-  flex: ${(props) => props.flex || '0 1 auto'};
-  align-items: ${(props) => props.alignItems || 'stretch'};
-  margin: ${(props) => props.margin || '0'};
-  padding: ${(props) => props.padding || '0'};
-  height: ${(props) => props.height || 'auto'};
-  max-width: ${(props) => props.maxWidth || 'none'};
+  justify-content: ${props => props.justifyContent || 'flex-start'};
+  flex-direction: ${props => props.flexDirection || 'row'};
+  flex-grow: ${props => props.flexGrow || 0};
+  flex-basis: ${props => props.flexBasis || 'auto'};
+  flex-shrink: ${props => props.flexShrink || 1};
+  flex-wrap: ${props => props.flexWrap || 'nowrap'};
+  flex: ${props => props.flex || '0 1 auto'};
+  align-items: ${props => props.alignItems || 'stretch'};
+  margin: ${props => props.margin || '0'};
+  padding: ${props => props.padding || '0'};
+  height: ${props => props.height || 'auto'};
+  max-width: ${props => props.maxWidth || 'none'};
+  width: ${props => props.width || 'initial'};
 `
