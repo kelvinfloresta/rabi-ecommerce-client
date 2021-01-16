e5261fa88bcb7157caa68845ac15a950e09c254a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-01-15 23:05:54 -0300

    feat(TableData): add expandable row support

--- src/frameworks/components/TableData.component.tsx
+++ src/frameworks/components/TableData.component.tsx
@@ -1,10 +1,12 @@
 import { ColumnsType } from 'antd/lib/table'
+import { ExpandableConfig } from 'antd/lib/table/interface'
 import React from 'react'
 
 import { StyledTable, IStyledTable } from './Table.style'
 
 export interface ITableDataProps<RecordType extends { id: string }> {
   dataSource: RecordType[]
+  expandable?: ExpandableConfig<RecordType>
   loading: boolean
   columns: ColumnsType<RecordType>
   error: any
@@ -14,6 +16,7 @@ export function TableData<RecordType extends { id: string }>({
   dataSource,
   loading,
   columns,
+  expandable,
 }: ITableDataProps<RecordType>) {
   const Table: IStyledTable<RecordType> = StyledTable
 
@@ -28,6 +31,7 @@ export function TableData<RecordType extends { id: string }>({
       }}
       columns={columns}
       dataSource={dataSource}
+      expandable={expandable}
     />
   )
 }
